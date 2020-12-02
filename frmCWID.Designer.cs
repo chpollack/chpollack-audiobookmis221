e831d9b90f6e07a625d89bba913ce253814bb7a1
chpollack <70770851+chpollack@users.noreply.github.com>
2020-12-02 11:53:57 -0800

    Add project files.

---
+++ frmCWID.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace AudioBookMIS_221
+{
+    partial class frmCWID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cwid = new System.Windows.Forms.Label();
+            this.txtCWID = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCLOSE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cwid
+            // 
+            this.cwid.AutoSize = true;
+            this.cwid.Location = new System.Drawing.Point(31, 33);
+            this.cwid.Name = "cwid";
+            this.cwid.Size = new System.Drawing.Size(158, 24);
+            this.cwid.TabIndex = 0;
+            this.cwid.Text = "Enter Your CWID:";
+            this.cwid.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtCWID
+            // 
+            this.txtCWID.Location = new System.Drawing.Point(247, 30);
+            this.txtCWID.Name = "txtCWID";
+            this.txtCWID.Size = new System.Drawing.Size(215, 29);
+            this.txtCWID.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(247, 79);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(92, 28);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCLOSE
+            // 
+            this.btnCLOSE.Location = new System.Drawing.Point(370, 79);
+            this.btnCLOSE.Name = "btnCLOSE";
+            this.btnCLOSE.Size = new System.Drawing.Size(92, 28);
+            this.btnCLOSE.TabIndex = 3;
+            this.btnCLOSE.Text = "Close";
+            this.btnCLOSE.UseVisualStyleBackColor = true;
+            this.btnCLOSE.Click += new System.EventHandler(this.btnCLOSE_Click);
+            // 
+            // frmCWID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 128);
+            this.Controls.Add(this.btnCLOSE);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtCWID);
+            this.Controls.Add(this.cwid);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "frmCWID";
+            this.Text = "CWID";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cwid;
+        private System.Windows.Forms.TextBox txtCWID;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCLOSE;
+    }
+}
+
